499d2cc310209660635215a18e53feaa9bd926a4
Andres Rios <rianby64@gmail.com>
2016-12-22 01:52:42 +0300

    [TEST] Add helper setupTemplateTest()

--- test/helper.js
+++ test/helper.js
@@ -15,3 +15,40 @@ function setupTest() {
     tree3: tree.Tree3
   };
 }
+
+function setupTemplateTest() {
+
+  var template = `
+    <ul id="demo-tree" class="mdl-tree3"></ul>
+    <template for="demo-tree">
+      <li class="mdl-menu__item
+                 mdl-tree3__contextmenu--action1">
+        Action 1
+      </li>
+      <li class="mdl-menu__item
+                 mdl-tree3__contextmenu--action2">
+        Action 2
+      </li>
+    </template>
+  `;
+  var range = new Range();
+  var clone = range.createContextualFragment(template);
+  document.body.appendChild(clone);
+
+  var tree = document.querySelector('#demo-tree');
+  componentHandler.upgradeElement(tree);
+  return {
+    tree: tree,
+    tree3: tree.Tree3,
+    actions: [
+      {
+        css: 'mdl-tree3__contextmenu--action1',
+        name: 'action1'
+      },
+      {
+        css: 'mdl-tree3__contextmenu--action2',
+        name: 'action2'
+      }
+    ]
+  };
+}
